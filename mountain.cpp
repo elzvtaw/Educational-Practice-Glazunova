08399b25318979acdd2641fff82cb37ca28fd7b3
lizza <lizza.irk@gmail.com>
2026-06-16 16:16:36 +0800

    Реализуем алгоритм дин. программирования для хранения сумм и создаём массив, чтобы запоминать откуда пришли

--- mountain.cpp
+++ mountain.cpp
@@ -12,5 +12,18 @@ int main() {
       std::cin >> mountain[i][j];
     }
   }
+  //  dp[i][j] - минимальная сумма от вершины до клетки (i,j)
+  std::vector<std::vector<int>> dp(N);
+  for (int i = 0; i < N; i++) {
+    dp[i].resize(i + 1, 1000000000);
+  }
+  //  Создаём массив, чтобы запоминать, откуда пришли
+  //  parent[i][j] - номер столбца, из которого пришли
+  std::vector<std::vector<int>> parent(N);
+  for (int i = 0; i < N; i++) {
+    parent[i].resize(i + 1, -1);
+  }
+  dp[0][0] = mountain[0][0];
+  parent[0][0] = -1;
   
 }
