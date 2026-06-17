5f52783e5cbeaf132824b1d6fca0a48a3353aef4
lizza <lizza.irk@gmail.com>
2026-06-17 18:07:34 +0800

    Вводим данные: n - длина перестановки (кол-во букв), k раз шифрования, p массив (перестановка), зашифрованное слово

---
+++ task_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+  int n = 0;
+  int k = 0;
+  std::cin >> n >> k;
+  std::vector<int> p(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> p[i];
+  }
+  std::string s;
+  std::cin >> s;
+
+}
