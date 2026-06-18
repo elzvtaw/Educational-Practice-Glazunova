257de1052466c137839abdc77896d6c039991ae3
lizza <lizza.irk@gmail.com>
2026-06-18 18:37:12 +0800

    Вводим данные согласно условию задачи: n, a, b, x, y и создаём переменную count для подсчёта максимального кол-ва работающих пар из микроконтроллера и модуля управления мотором

---
+++ task_6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+  int n = 0;
+  std::cin >> n;
+  for (int i = 0; i < n; ++i) {
+    int a = 0;
+    int b = 0;
+    int x = 0;
+    int y = 0;
+    std::cin >> a >> b >> x >> y;
+    int count = 0;
+    
+  } 
+}
