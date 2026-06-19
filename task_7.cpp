6d30a45e06785674c59929e5fe6239791d901069
lizza <lizza.irk@gmail.com>
2026-06-19 23:56:23 +0800

    Если строка соответствует требуемому условию, то выводим YES, иначе - NO

--- task_7.cpp
+++ task_7.cpp
@@ -8,6 +8,10 @@ int main() {
   if (number.length() == 6 and isupper(number[0]) and isdigit(number[1]) and isdigit(number[2]) and isdigit(number[3]) and isupper(number[4]) and isupper(number[5])) {
     //  isupper() проверяет является ли символ заглавной
     //  isdigit() проверяет является ли символ цифрой
-  
+    std::cout << "YES" << std::endl;
+  } else {
+    std::cout << "NO" << std::endl;
   }
+  return 0
 }
+
