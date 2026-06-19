4cbd3b76adf1878aecd3a6963a57690ea56c8258
lizza <lizza.irk@gmail.com>
2026-06-19 23:54:26 +0800

    Проверяем длину строки (необхоодимо чтобы она была из 6 символов), проверяем, чтобы 1-й и последние 2 символа были заглавными буквами, а оставшиееся 3 - цифрами. Используем функции isupper() и isdigit() для проверки

--- task_7.cpp
+++ task_7.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 int main() {
   std::string number;
   std::cin >> number;
+  if (number.length() == 6 and isupper(number[0]) and isdigit(number[1]) and isdigit(number[2]) and isdigit(number[3]) and isupper(number[4]) and isupper(number[5])) {
+    //  isupper() проверяет является ли символ заглавной
+    //  isdigit() проверяет является ли символ цифрой
   
+  }
 }
