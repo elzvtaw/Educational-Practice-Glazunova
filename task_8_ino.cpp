84323f3848964516fb5ffa64f077d561ce74d593
lizza <lizza.irk@gmail.com>
2026-06-21 00:06:58 +0800

    Создаём функцию для отображения фиолетового круга

--- task_8_ino.cpp
+++ task_8_ino.cpp
@@ -25,3 +25,32 @@ void setPixelXY(int x, int y, uint32_t color) {
     strip.setPixelColor(getPixelIndex(x, y), color);
   }
 }
+
+// Фиолетовый круг
+void showPurpleCircle(unsigned long duration) {
+  uint8_t circle[8][8] = {
+    {0,0,1,1,1,1,0,0},
+    {0,1,1,1,1,1,1,0},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {0,1,1,1,1,1,1,0},
+    {0,0,1,1,1,1,0,0}
+  };
+
+  unsigned long start = millis();
+  while (millis() - start < duration * 1000) {
+    for (int y = 0; y < 8; y++) {
+      for (int x = 0; x < 8; x++) {
+        if (circle[y][x] == 1) {
+          setPixelXY(x, y, strip.Color(128, 0, 255));  // Фиолетовый
+        } else {
+          setPixelXY(x, y, strip.Color(0, 0, 0));
+        }
+      }
+    }
+    strip.show();
+    delay(50);
+  }
+}
