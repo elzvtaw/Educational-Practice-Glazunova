24f91026f50950f59940b081e625effd097a4ec9
lizza <lizza.irk@gmail.com>
2026-06-23 02:23:48 +0800

    Обрабатываем наше изображение: размываем и переводим в HSV

--- task_11.py
+++ task_11.py
@@ -3,3 +3,9 @@ import numpy as np
 
 # Загрузка изображения
 image = cv2.imread("C:/Users/lizza/first-task-practice/test1_task_11.jpg")
+
+# Размытие для подавления шума
+blurred = cv2.GaussianBlur(image, (11, 11), 0)
+
+# Перевод в HSV
+hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
