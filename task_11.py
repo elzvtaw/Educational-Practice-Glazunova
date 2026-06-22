54510dd727c3b638449cfcba0be1a437c6f0db3c
lizza <lizza.irk@gmail.com>
2026-06-23 02:32:02 +0800

    Добавляем диапазон для синего цвета в HSV и создаём маску для синего цвета, находим контуры

--- task_11.py
+++ task_11.py
@@ -9,3 +9,13 @@ blurred = cv2.GaussianBlur(image, (11, 11), 0)
 
 # Перевод в HSV
 hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+# Диапазон синего цвета в HSV
+darkblue_min = np.array((100, 80, 80), np.uint8) # Нижняя граница синего
+darkblue_max = np.array((140, 255, 255), np.uint8) # Верхняя граница синего
+
+# Создание маски для синего цвета
+darkblue_mask = cv2.inRange(hsv, darkblue_min, darkblue_max)
+
+# Нахождение контуров
+contours, hierarchy = cv2.findContours(darkblue_mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
